068c812feeb768478535c8bd638f3a4551f6d9cc
Lakis <lamoschos@mailbox.org>
2022-12-11 23:01:01 +0200

    added social media_2

--- src/App.jsx
+++ src/App.jsx
@@ -51,8 +51,8 @@ function App()
             <a href="#" className="mx-3 hover:opacity-80 duration-150">About Us</a> |  
             <a href="#" className="mx-3 hover:opacity-80 duration-150"> Privacy</a> | 
             <a href="#" className="mx-3 hover:opacity-80 duration-150"> Contact</a> 
-			<MdFacebook />
           </div>
+		  <MdFacebook />
 
         </div>
 
