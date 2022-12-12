b38c3cf417bde4a99f24b1d491cca84afbf22441
Lakis <lamoschos@mailbox.org>
2022-12-13 00:46:29 +0200

    text orange

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { AiFillFacebook } from "react-icons/ai"
 function App() 
 {
   return (
-    <div className='min-h-screen flex flex-col text-orange'>
+    <div className='min-h-screen flex flex-col text-orange-300'>
       <main className="container mx-auto px-6 pt-16 flex-1 text-center">
         <h2 className="text-2xl md:text-4xl lg:text-6xl uppercase">Φθυνη | Γρηγορη | Ασφαλες</h2>
 				<h1 className="text-3xl md:text-6xl lg:text-8xl uppercase font-black mb-8">Μετακoμιση</h1>
