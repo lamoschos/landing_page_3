6a94a7e48ff3d464af53d55bef5962b5c7b0429a
Lakis <lamoschos@mailbox.org>
2022-12-13 01:01:36 +0200

    text blue

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { AiFillFacebook } from "react-icons/ai"
 function App() 
 {
   return (
-    <div className='min-h-screen flex flex-col text-blue-700'>
+    <div className='min-h-screen flex flex-col text-blue-800'>
       <main className="container mx-auto px-6 pt-16 flex-1 text-center">
         <h2 className="text-2xl md:text-4xl lg:text-6xl uppercase">Φθυνη | Γρηγορη | Ασφαλες</h2>
 				<h1 className="text-3xl md:text-6xl lg:text-8xl uppercase font-black mb-8">Μετακoμιση</h1>
